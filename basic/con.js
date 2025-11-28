4e169197dceb374b288cdf12f471f8fffa8f3d8f
bipinpantha74 <bipinpantha74@gmail.com>
2025-11-28 14:31:36 +0000

    bipin

---
+++ basic/con.js
@@ -0,0 +1,19 @@
+// let score=" 123abc"
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+
+
+// let isLoggedIn="bipin"
+// let bipinisloggedin=Number(isLoggedIn)
+// console.log(bipinisloggedin)
+
+
+// let number=3
+// let negValue=-number
+// console.log(negValue)
+console.log(1+2+3+"4" )
