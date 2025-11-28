401bc5bed4ee69a2d618c565f11535996acf021a
bipinpantha74 <bipinpantha74@gmail.com>
2025-11-28 10:11:35 +0000

    helllo

---
+++ basic/hello.js
@@ -0,0 +1,13 @@
+ const accountId= "bipindai"
+ let password="bbbb"
+ var email=" bipin.google.com"
+ accountCity="kathmandu"
+
+//  accountId="3"
+ console.log(accountId);
+ password="12345"
+ email="bipin"
+ accountCity="nepal"
+ console.table([accountId,password,email,accountCity])
+//  console.log(typeof password);
+ 
