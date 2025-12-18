1251b1daf9072a60ddfedb45405f68733528f13c
bipinpantha74 <bipinpantha74@gmail.com>
2025-12-18 10:48:05 +0000

    bipin dai

--- bipin/hero.js
+++ bipin/hero.js
@@ -1,2 +1,43 @@
-const bipin=[1,4,5,7]
-console.log(bipin[3])
+// function bipin(username){
+//     return`${username}just logged in`
+// }
+// console.log(bipin("hari"))
+
+
+// function mina(username){
+//     if (username===undefined){
+//         console.log("please enter a username");
+        
+//         return 
+//     }
+//     return `${username}just logged in`
+// }
+//  console.log(mina("hwllo"))//if there is not any double comma it will follow the loop//
+ 
+// function mina(username){
+//     if (!username){
+//         console.log("please enter a username");
+        
+//         return 
+//     }
+//     return `${username}just logged in`
+// }
+//  console.log(mina())//best way to do this ! did the opposite
+function CalculateCartPrice(...num1){
+    return num1
+
+}
+// console.log(CalculateCartPrice(2220,400,500));
+const user={
+    username:"bipin",
+    surname:"pantha"
+}
+function daidonho(anyObj){
+    console.log(`usename is ${anyObj.username } and his surname is ${anyObj.surname}`);
+    
+
+}
+daidonho(user)
+
+
+
---
+++ bipin/kxa.js
@@ -0,0 +1,11 @@
+function  bipin(){
+    const usernames="bipindai"
+    // console.log(website) this cannot be excess
+    function two(){
+        const website="amazon"
+        console.log(usernames);
+        
+    }
+    two()
+}
+bipin()
